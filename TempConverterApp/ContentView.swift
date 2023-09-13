9c868ee078d92208a3953aa3043b430b9b30744d
Bakhtiyor Mirzo <82373830+bakhtiyormirzo@users.noreply.github.com>
2023-09-12 21:59:06 -0400

    Initial Commit

---
+++ TempConverterApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TempConverterApp
+//
+//  Created by Bakhtiyorjon Mirzajonov on 12/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TempConverterApp/TempConverterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempConverterAppApp.swift
+//  TempConverterApp
+//
+//  Created by Bakhtiyorjon Mirzajonov on 12/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TempConverterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
