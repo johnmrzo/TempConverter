c7cba13542c17519c42f65727b3fd4d4d2caebd3
Bakhtiyor Mirzo <82373830+bakhtiyormirzo@users.noreply.github.com>
2023-09-12 23:15:43 -0400

    skleton of the UI is set

--- TempConverterApp/Views/ContentView.swift
+++ TempConverterApp/Views/ContentView.swift
@@ -8,15 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+  
+  @State private var Value = ""
+  
+  
+  var body: some View {
+    VStack {
+      Spacer()
+      
+      HStack {
+        Text("Temp")
+        Text("ºF")
+      }
+      
+      Spacer()
+      
+      Text("Enter Temperature:")
+      
+      HStack {
+        TextField("Temp", text: $Value)
+          .frame(width: 90.0)
+          .multilineTextAlignment(.center)
+          Text("ºC")
         }
-        .padding()
+      
+      Spacer()
+
+      Button("Convert") {
+        
+      }
+      Spacer()
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
